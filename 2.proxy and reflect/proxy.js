95bb24bd77ace46d705331c52dc0abfc68597ba7
tlkhanh <longkhanhta@gmail.com>
2023-11-12 15:39:58 +0700

    reflect

--- 2.proxy and reflect/proxy.js
+++ 2.proxy and reflect/proxy.js
@@ -1,10 +1,14 @@
 let product = { price: 5, quantity: 2 }
-// let proxiedProduct = new Proxy(product, {})
 let proxiedProduct = new Proxy(product, {
-    get(target, key) {
+    get(target, key, receiver) {
         console.log('Get was called with key = ', key);
-        // return target[key]
-        return Reflect.get(target, key)
+        return Reflect.get(...arguments)
+    },
+    set(target, key, value, receiver) {
+        console.log('Set was called with key = ', key, 'and value = ', value);
+        return Reflect.set(...arguments);
     }
 })
+
+proxiedProduct.price = 12
 console.log(proxiedProduct.price);
