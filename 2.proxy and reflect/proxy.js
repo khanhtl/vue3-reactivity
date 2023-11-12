bea4f75df14ad360471f3e987ae01475573091d1
tlkhanh <longkhanhta@gmail.com>
2023-11-12 15:50:16 +0700

    auto trigger

--- 2.proxy and reflect/proxy.js
+++ 2.proxy and reflect/proxy.js
@@ -1,17 +1,51 @@
-function reactive(target) {
-    const handler = {
-        get(target, key, receiver) {
-            console.log('Get was called with key = ', key);
-            return Reflect.get(...arguments)
-        },
-        set(target, key, value, receiver) {
-            console.log('Set was called with key = ', key, 'and value = ', value);
-            return Reflect.set(...arguments);
-        }
-    }
-    return new Proxy(target, handler);
-}
-let proxiedProduct = reactive({ price: 5, quantity: 2 })
+const targetMap = new WeakMap();
+const product = reactive({ price: 5, quantity: 2 });
+let total = product.price * product.quantity;
 
-proxiedProduct.price = 12
-console.log(proxiedProduct.price);
+function effect() {
+  total = product.price * product.quantity;
+}
+
+function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+  dep.add(effect);
+}
+function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  let dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach((effect) => effect());
+  }
+}
+
+function reactive(target) {
+  const handler = {
+    get(target, key, receiver) {
+      let result = Reflect.get(...arguments);
+      track(target, key);
+      return result;
+    },
+      set(target, key, value, receiver) {
+          let oldValue = target[key];
+          let result = Reflect.set(...arguments);
+          if (result && oldValue != value) {
+              trigger(target, key);
+          }
+      return result;
+    },
+  };
+  return new Proxy(target, handler);
+}
+
+console.log(total);
+product.price = 12;
+console.log(total);
+console.log(product.price);
