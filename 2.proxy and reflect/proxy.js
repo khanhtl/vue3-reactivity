30110606c6ac9ffce3351e744e1d767dc8a28828
tlkhanh <longkhanhta@gmail.com>
2023-11-12 15:41:34 +0700

    reactive

--- 2.proxy and reflect/proxy.js
+++ 2.proxy and reflect/proxy.js
@@ -1,14 +1,17 @@
-let product = { price: 5, quantity: 2 }
-let proxiedProduct = new Proxy(product, {
-    get(target, key, receiver) {
-        console.log('Get was called with key = ', key);
-        return Reflect.get(...arguments)
-    },
-    set(target, key, value, receiver) {
-        console.log('Set was called with key = ', key, 'and value = ', value);
-        return Reflect.set(...arguments);
+function reactive(target) {
+    const handler = {
+        get(target, key, receiver) {
+            console.log('Get was called with key = ', key);
+            return Reflect.get(...arguments)
+        },
+        set(target, key, value, receiver) {
+            console.log('Set was called with key = ', key, 'and value = ', value);
+            return Reflect.set(...arguments);
+        }
     }
-})
+    return new Proxy(target, handler);
+}
+let proxiedProduct = reactive({ price: 5, quantity: 2 })
 
 proxiedProduct.price = 12
 console.log(proxiedProduct.price);
